072d8a71fa0df02b352a4d1de0bc19713aaeedcf
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-09 10:37:45 +0100

    CREATE: SASS added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,19 @@
 import { useEffect }from 'react'
-
+import './App.scss';
+import SeacrhIcon from './search.svg';
 
 // 9e8d5592
 
 const API_URL = 'http://www.omdbapi.com?apikey=9e8d5592';
+
+
+const Movies = {
+  "Title": "Batman v Superman: Dawn of Justice (Ultimate Edition)",
+  "Year": "2016",
+  "imdbID": "tt18689424",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOTRlNWQwM2ItNjkyZC00MGI3LThkYjktZmE5N2FlMzcyNTIyXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg"
+}
 
 const App = () => {
 
@@ -21,7 +31,24 @@ useEffect(() => {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className='app'>
+        <h1>MOVIE-LAND</h1>
+
+        <div className="search">
+          <input placeholder='Search for movies' value={'batman'} onChange={() => {
+
+          }} 
+          />
+          <img src={SeacrhIcon} alt='search icon'
+           onClick={() => {
+
+           }}
+           />
+        </div>
+        <div className="container">
+
+        </div>
+      </div>
     </div>
   )
 }
